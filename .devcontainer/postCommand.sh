ce88870367923f61d558d7fc3b5eabfea5e410cd
lcharleux <ludovic.charleux@univ-smb.fr>
2022-06-13 15:50:11 +0000

    Updated postCommand.sh

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -4,4 +4,5 @@ echo "Start Devcontainer post-command process !"
 cd ../automatic_university_scheduler/
 echo "Installing pre-commit"
 /opt/conda/envs/ortools/bin/pre-commit install
+/opt/conda/envs/ortools/bin/pre-commit install-hooks
 echo "Devcontainer post-command process done !"
