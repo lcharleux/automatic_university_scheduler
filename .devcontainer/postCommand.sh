0e76b64bbb5a52a2b62659c4b42a4121f7ab8356
lcharleux <ludovic.charleux@univ-smb.fr>
2022-06-14 08:57:28 +0000

    Updated docker definitions

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -3,6 +3,9 @@ echo "Start Devcontainer post-command process !"
 
 cd ../automatic_university_scheduler/
 echo "Installing pre-commit"
-/opt/conda/envs/ortools/bin/pre-commit install
-/opt/conda/envs/ortools/bin/pre-commit install-hooks
+# /opt/conda/envs/ortools/bin/pre-commit install
+# /opt/conda/envs/ortools/bin/pre-commit install-hooks
+echo $CONDA_ENV
+${CONDA_ENV}pre-commit install
+${CONDA_ENV}pre-commit install-hooks
 echo "Devcontainer post-command process done !"
